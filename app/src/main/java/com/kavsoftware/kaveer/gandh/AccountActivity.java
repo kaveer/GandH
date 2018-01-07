f3ccb033bed2d925a1734942be7d2ba962389b27
kaveer <kaveer.rajcoomar@gmail.com>
2018-01-07 22:04:53 +0400

    Add structure for database

--- app/src/main/java/com/kavsoftware/kaveer/gandh/AccountActivity.java
+++ app/src/main/java/com/kavsoftware/kaveer/gandh/AccountActivity.java
@@ -9,5 +9,11 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        //check android db for credential
+        //if found take credentital to generate access token
+        //navigate to main screen
+        //if not found
+        // navigate to login screen
     }
 }
